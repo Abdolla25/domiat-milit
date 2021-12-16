c70ddbeaa2050b162a7a07e06ef130042b62ba06
Abdolla25 <abdollaz225@gmail.com>
2021-12-16 14:29:46 +0200

    add filter to tables

---
+++ js/table-filter.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.btn-filter').on('click', function () {
+      var $target = $(this).data('target');
+      if ($target != 'all') {
+        $('.table-removable').css('display', 'none');
+        $('.table-removable[data-status="' + $target + '"]').fadeIn('slow');
+      } else {
+        $('.table-removable').css('display', 'none').fadeIn('slow');
+      }
+    });
+
+ });
