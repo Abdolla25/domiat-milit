58f73548cf9baeba24102fd1ce80fec77b894e88
Abdolla25 <abdollaz225@gmail.com>
2022-02-25 10:09:52 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 set -e
-# npm run build
+npm run build
 cd dist
+cp index.html 404.html
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:Abdolla25/domiat-milit.git master:gh-pages
 cd -
+
+# bash deploy.sh
